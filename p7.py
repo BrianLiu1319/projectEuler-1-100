9e2c8663a3f8338efdf3bd7fed0368b00c0c9a88
BrianLiu1319 <brianliu1337@yahoo.com>
2022-04-06 10:07:34 -0700

    idk

---
+++ p7.py
@@ -0,0 +1,17 @@
+import math
+# 50th prime = 229
+
+
+def check_prime(num):
+    for x in range(1,int(math.sqrt(num))):
+        if(num % x) == 0:
+            return False
+    return True
+
+
+count = 1
+prime_list = []
+while len(prime_list) != 50:
+    if(check_prime(count)):
+        prime_list.append(count)
+    count += 1
